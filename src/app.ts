42fa9057e8db905e35ecb6ef216ed73cc92ce5e6
Omair Mohammed <a774922560@gmail.com>
2026-01-14 00:20:32 +0300

    fix(api): enable CORS for hasif web origin

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db";
 import wordsRoutes from "./modules/words/words.routes";
 import errorMiddleware from "./middlewares/error.middleware";
@@ -8,6 +9,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// app.use(
+//   cors({
+//     origin: ["https://hasiif.vercel.app"], // دومين الويب حقك
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 
